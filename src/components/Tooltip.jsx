a2f8b9eeb7beb8b9cec82d2249ef65208f564e89
Jackson Kary <kary0014@umn.edu>
2024-09-24 00:31:19 -0500

    removed comments

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -11,18 +11,12 @@ const Tooltip = ({ text, children }) => {
         >
             {children}
             <div
-                // mb-2
                 className={`absolute bottom-full left-1/2 transform -translate-x-1/2 mb-1.5 transition-opacity duration-100 ${
                     isHovered ? 'opacity-100' : 'opacity-0'
                 }`}
             >
-                {/* text-sm */}
-                {/* REMOVE font-semibold */}
                 <div className="relative bg-stone-800 text-stone-200 text-xs font-semibold rounded-lg shadow-lg px-2 py-1">
                     {text}
-                    {/* border-t-red-800 */}
-                    {/* border-t-stone-500 */}
-                    {/* border-t-8 */}
                     <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-full w-0 h-0 border-t-4 border-t-red-800 border-x-8 border-x-transparent"></div>
                 </div>
             </div>
