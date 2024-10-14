4e29c37aa8a3e16265565d620ac2fede6e0e2c57
Jackson Kary <kary0014@umn.edu>
2024-10-13 23:59:38 -0500

    removed vertical margin

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -1,6 +1,6 @@
 function Divider() {
   return (
-    <div className={`mx-auto w-full relative my-8 flex justify-center items-center`}>
+    <div className={`mx-auto w-full relative flex justify-center items-center`}>
       <div className={`absolute w-full h-0.5 bg-gradient-to-r from-stone-300 via-stone-500 to-stone-300`}></div>
       <div className={`w-1.5 h-1.5 rounded-full bg-stone-500 absolute left-0`}></div>
       <div className={`w-1.5 h-1.5 rounded-full bg-stone-500 absolute right-0`}></div>
