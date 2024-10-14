09d42854a24fdf79f7cbe31f667d3ecef19c4ab1
Jackson Kary <kary0014@umn.edu>
2024-10-14 00:19:54 -0500

    new custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,22 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        // Custom breakpoints
+        // Viewport Width Breakpoints
         'xl+': '1445px',
         'md+': '875px',
         'xs': '540px',
-        'xxs': '400px'
+        'xxs': '400px',
+        'xxxs': '320px',
+      },
+      padding: {
+        '1.5': '6px',
+        'vw-2': '2vw',
+        'vw-4': '4vw',
+        'vw-6': '6vw',
+        'vw-8': '8vw',
+        'vw-11': '11vw',
+        'vw-15': '15vw',
+        'vw-20': '20vw',
       },
       margin: {
         // Used as left margin for signature img
@@ -24,6 +35,48 @@ module.exports = {
         'vw-8': '8vw',
         'vw-11': '11vw',
         'vw-15': '15vw',
+        'vw-20': '20vw',
+        // Viewport Height
+        '5vh': '5vh',
+        '10vh': '10vh',
+        '15vh': '15vh',
+        '20vh': '20vh',
+        '25vh': '25vh',
+        '30vh': '30vh',
+        '40vh': '40vh',
+        '50vh': '50vh',
+        '60vh': '60vh',
+        '70vh': '70vh',
+        '80vh': '80vh',
+        '90vh': '90vh',
+        '100vh': '100vh',
+        // Small Viewport Height
+        '5svh': '5svh',
+        '10svh': '10svh',
+        '15svh': '15svh',
+        '20svh': '20svh',
+        '25svh': '25svh',
+        '30svh': '30svh',
+        '40svh': '40svh',
+        '50svh': '50svh',
+        '60svh': '60svh',
+        '70svh': '70svh',
+        '80svh': '80svh',
+        '90svh': '90svh',
+        '100svh': '100svh',
+      },
+      height: {
+        '70svh': '70svh',
+        '80svh': '80svh',
+        '90svh': '90svh',
+        '100svh': '100svh',
+        '70vh': '70vh',
+        '80vh': '80vh',
+        '90vh': '90vh',
+        '100vh': '100vh',
+      },
+      minHeight: {
+        '100svh': '100svh', 
       },
       borderWidth: {
         '6': '6px',
